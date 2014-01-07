bc33b8e0dc6811da7900bb2865d3ea17a3e2cf49
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-01-07 21:31:00 +0900

    fileリソースを分離

--- modules/ntp/manifests/config.pp
+++ modules/ntp/manifests/config.pp
@@ -1,15 +1,16 @@
 class ntp::config {
 
-  file {
-    '/etc/ntp.conf':
-      ensure  => present,
-      content => 'puppet:///modules/ntp/etc/ntp.conf';
+  file { '/etc/ntp.conf':
+    ensure  => present,
+    content => 'puppet:///modules/ntp/etc/ntp.conf',
+  }
 
-    '/etc/ntp.d':
-      ensure => directory;
+  file { '/etc/ntp.d':
+    ensure => directory,
+  }
 
-    '/etc/ntp.d/drift':
-      ensure => present;
+  file { '/etc/ntp.d/drift':
+    ensure => present,
   }
 
 }
