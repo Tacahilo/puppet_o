e709505d481f826ccc1e7d4c65fd244c5d6c77df
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-01-18 03:52:37 +0900

    nginx moduleにサービス定義を追加

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -3,9 +3,11 @@ class nginx {
   include nginx::install
   include nginx::yumrepo
   include nginx::file
+  include nginx::service
 
   Class['nginx::yumrepo']
   -> Class['nginx::install']
   -> Class['nginx::file']
+  -> Class['nginx::service']
 
 }
