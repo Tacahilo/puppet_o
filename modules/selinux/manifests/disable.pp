877813a7ea53ebae4668dd02d16213674d19644b
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-20 02:57:49 +0900

    selinuxの無効化するだけに2ファイルも不要

--- modules/selinux/manifests/disable.pp
+++
@@ -1,9 +0,0 @@
-class selinux::disable {
-
-  file { '/etc/selinux/config':
-    ensure  => file,
-    mode    => '0644',
-    content => 'SELINUX=disabled',
-  }
-
-}
--- modules/selinux/manifests/init.pp
+++ modules/selinux/manifests/init.pp
@@ -1,5 +1,15 @@
 class selinux {
 
-  include selinux::disable
+  file { '/etc/selinux/config':
+    ensure  => file,
+    mode    => '0644',
+    content => 'SELINUX=disabled',
+  }
+
+  exec { 'disable selinux':
+    path    => '/bin:/usr/sbin',
+    command => 'setenfore 0',
+    unless  => 'getenforce | grep -q Disabled',
+  }
 
 }
