4504c4e214b9cddeb8dd052d1fa7bced4ab278b9
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-20 02:51:52 +0900

    anchor pattn変更

--- roles/sc/manifests/init.pp
+++ roles/sc/manifests/init.pp
@@ -1,21 +1,11 @@
 class sc {
 
-  include ::base
-  include ::munin
-  include ::sc::package
-  include ::sc::file
-  include ::sc::xbuild
-  include ::sc::nginx
-  include ::sc::wiki
-  include ::sc::znc
-
-     Class['::base']
-  -> Class['::munin']
-  -> Class['::sc::package']
-  -> Class['::sc::file']
-  -> Class['::sc::xbuild']
-  -> Class['::sc::nginx']
-  -> Class['::sc::wiki']
-  -> Class['::sc::znc']
-
+     class{'::base':}
+  -> class{'::munin':}
+  -> class{'sc::package':}
+  -> class{'sc::file':}
+  -> class{'sc::xbuild':}
+  -> class{'sc::nginx':}
+  -> class{'sc::wiki':}
+  -> class{'sc::znc':}
 }
