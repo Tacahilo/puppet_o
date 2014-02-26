15a0cb93ef4fc1d31b8e8316766b0634e86a94d1
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-02-26 09:52:46 +0900

    strengthen iptables

--- roles/sc/files/usr/local/bin/iptabels.sh
+++ roles/sc/files/usr/local/bin/iptabels.sh
@@ -16,6 +16,9 @@ LOCALNET=$LOCALNET_ADDR/$LOCALNET_MASK
 # start configuration                   #
 #---------------------------------------#
 
+#---------------------------------------#
+# general rule                          #
+#---------------------------------------#
 iptables -P INPUT   DROP
 iptables -P OUTPUT  ACCEPT
 iptables -P FORWARD DROP
@@ -24,13 +27,39 @@ iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -s $LOCALNET -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -d 255.255.255.255 -j DROP
+iptables -A INPUT -d 224.0.0.1 -j DROP
+
+#---------------------------------------#
+# protection for attacks                #
+#---------------------------------------#
+sysctl -w net.ipv4.tcp_syncookies=1 > /dev/null
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
+echo "net.ipv4.tcp_syncookies=1" >> /etc/sysctl.conf
+
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1 > /dev/null
+sed -i '/net.ipv4.icmp_echo_ignore_broadcasts/d' /etc/sysctl.conf
+echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.conf
+
+sed -i '/net.ipv4.conf.*.accept_source_route/d' /etc/sysctl.conf
+for dev in `ls /proc/sys/net/ipv4/conf/`
+do
+    sysctl -w net.ipv4.conf.$dev.accept_source_route=0 > /dev/null
+    echo "net.ipv4.conf.$dev.accept_source_route=0" >> /etc/sysctl.conf
+done
+
+iptables -N LOG_PINGDEATH
+iptables -A LOG_PINGDEATH -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+iptables -A LOG_PINGDEATH -j LOG --log-prefix '[IPTABLES PINGDEATH] : '
+iptables -A LOG_PINGDEATH -j DROP
+iptables -A INPUT -p icmp --icmp-type echo-request -j LOG_PINGDEATH
 
 #----------------------------------------------------------#
 # port                                                     #
 #----------------------------------------------------------#
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport SSH -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -s 192.30.252.0/22 -p tcp --dport 50000 -j ACCEPT
+iptables -A INPUT -s 192.30.252.0/22 -p tcp --dport GITHUB -j ACCEPT
 
 # save and start
 /etc/rc.d/init.d/iptables save
