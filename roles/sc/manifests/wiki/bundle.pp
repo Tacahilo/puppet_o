53cc9b5b508ef134f4916afce24e1b52112e5c7e
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-03-02 07:16:15 +0900

    version up

--- roles/sc/manifests/wiki/bundle.pp
+++ roles/sc/manifests/wiki/bundle.pp
@@ -3,7 +3,7 @@ class sc::wiki::bundle {
   $path = '/var/www/wiki'
 
   exec { 'bundle_install_for_wiki':
-    path    => '/usr/local/ruby-2.1.0/bin:/bin:/usr/bin:/usr/local/bin',
+    path    => '/usr/local/ruby-2.1.1/bin:/bin:/usr/bin:/usr/local/bin',
     cwd     => $path,
     command => 'bundle install --path vendor',
     unless  => 'bundle check',
