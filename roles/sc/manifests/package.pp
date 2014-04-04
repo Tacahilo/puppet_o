8880fd69e5c3171678d02e3d5caa81b522814845
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-04-05 05:08:44 +0900

    add perl package for git

--- roles/sc/manifests/package.pp
+++ roles/sc/manifests/package.pp
@@ -3,8 +3,10 @@ class sc::package {
   include ::clamd
 
   $packages = [
+    'gettext', # for git
     'libicu-devel',
     'perl-ExtUtils-MakeMaker', # for git
+    'perl-Locale-Msgfmt', # for git
     'spawn-fcgi',
   ]
 
